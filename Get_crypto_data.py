63a41ae302596dfb6c18f46c542e2f69b4c42d3c
Julian Wössner <julianwossner@eduroamstud-130-82-253-164.unisg.ch>
2019-11-12 11:24:26 +0100

    final

--- Get_crypto_data.py
+++ Get_crypto_data.py
@@ -71,12 +71,12 @@ def get_json_data(json_url, cache_path):
 
 base_polo_url = 'https://poloniex.com/public?command=returnChartData&currencyPair={}&start={}&end={}&period={}'
 start_date = time.mktime(datetime(2015,1,1).timetuple()) # get data from the start of 2015 in seconds
-end_date = time.mktime(datetime(2019,10,30)).timetuple()) # up until today
-pediod = 86400 # pull daily data (86,400 seconds per day)
+end_date = time.mktime(datetime(2019,10,30).timetuple()) # up until today
+period = 86400 # pull daily data (86,400 seconds per day)
 
 def get_crypto_data(poloniex_pair):
     '''Retrieve cryptocurrency data from poloniex'''
-    json_url = base_polo_url.format(poloniex_pair, start_date.date(), end_date.date(), pediod)
+    json_url = base_polo_url.format(poloniex_pair, start_date, end_date, period)
     data_df = get_json_data(json_url, poloniex_pair)
     data_df = data_df.set_index('date')
     return data_df
