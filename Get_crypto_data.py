bf884743a87abfd93c7291266a464e49d7bca620
jm2802 <48387073+jm2802@users.noreply.github.com>
2019-11-12 11:12:12 +0100

    Update Get_crypto_data.py

--- Get_crypto_data.py
+++ Get_crypto_data.py
@@ -17,7 +17,7 @@ import pandas as pd
 import pickle
 import quandl
 from datetime import datetime
-
+import time
 from pandas import DataFrame
 import plotly.offline as py
 import plotly.graph_objs as go
@@ -70,8 +70,8 @@ def get_json_data(json_url, cache_path):
     return df
 
 base_polo_url = 'https://poloniex.com/public?command=returnChartData&currencyPair={}&start={}&end={}&period={}'
-start_date = datetime.strptime('2015-01-01', '%Y-%m-%d') # get data from the start of 2015
-end_date = datetime.strptime('2019-10-30', '%Y-%m-%d') # up until today
+start_date = time.mktime(datetime(2015,1,1).timetuple()) # get data from the start of 2015 in seconds
+end_date = time.mktime(datetime(2019,10,30)).timetuple()) # up until today
 pediod = 86400 # pull daily data (86,400 seconds per day)
 
 def get_crypto_data(poloniex_pair):
